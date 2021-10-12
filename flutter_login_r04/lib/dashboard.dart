950011dd2b2d5804e1fe09097bcb779055fea5ac
EDISAPUTRA\LENOVO <edisaputra@unja.ac.id>
2021-10-12 09:52:22 +0700

    tambah drawer

--- flutter_login_r04/lib/dashboard.dart
+++ flutter_login_r04/lib/dashboard.dart
@@ -10,6 +10,30 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Dashboard')),
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Welcome User', style: TextStyle(fontSize: 24)),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Log out'),
+            onTap: () {
+              Navigator.pushNamed(context, '/login');
+            },
+          )
+        ],
+      )),
       body: Container(
         constraints: BoxConstraints.expand(),
         // is used to create container full screen with filled content.
